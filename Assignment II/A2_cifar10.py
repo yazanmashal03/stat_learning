7e9ccab61ccd65662ac962ccdbddcc4b2681c30b
LarsdeHoop <134620883+LarsdeHoop@users.noreply.github.com>
2025-03-27 15:17:03 +0100

    Added 2b and 2c

--- Assignment II/A2_cifar10.py
+++ Assignment II/A2_cifar10.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 def unpickle(file):
     import pickle
@@ -15,6 +16,7 @@ dict5 = unpickle("./data/cifar-10-batches-py/data_batch_5")
 test = unpickle("./data/cifar-10-batches-py/test_batch")
 meta_data = unpickle("./data/cifar-10-batches-py/batches.meta")
 label_names = meta_data["label_names"]
+print(label_names)
 
 
 X_train = np.concatenate((dict1["data"],dict2["data"],dict3["data"],dict4["data"],dict5["data"]))
@@ -32,5 +34,31 @@ def plot_image(image, title=""):
     plt.show()
 
 # as a verification that everything is working correctly, plot an image
-image_nr = 320
+image_nr = 10
 plot_image(X_train[image_nr,:],label_names[y_train[image_nr]])
+
+## B - Fitting a logistic Regression model
+model = linear_model.LogisticRegression(penalty="l2",C=1e-9).fit(X_train, y_train)
+result = model.predict(X_test)
+print(sum(result == y_test))
+print(len(y_test))
+print(X_train.shape)
+
+print(label_names[0])
+
+## C - Crossvalidation
+# note that in the choosing of folds, there is some randomness
+X_train = X_train[:100,:]
+y_train = y_train[:100]
+K = 4
+C_list = [1e-7,1e-8,1e-9,1e-10]
+model = linear_model.LogisticRegressionCV(cv = K, Cs = C_list).fit(X_train,y_train)
+fold_accuracies = model.scores_[0]
+mean_accuracies = np.mean(fold_accuracies, axis=0)
+print(fold_accuracies)
+print(mean_accuracies)
+
+plt.figure()
+plt.loglog(C_list,mean_accuracies)
+plt.show()
+
