fad0815eaede41f26b114ce8f2f45694b5f63585
LucasVerbeeke <106536718+LucasVerbeeke@users.noreply.github.com>
2025-03-28 12:08:46 +0100

    Update A2_cifar10.py

--- Assignment II/A2_cifar10.py
+++ Assignment II/A2_cifar10.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn import model_selection
+from sklearn import metrics
 
 def unpickle(file):
     import pickle
@@ -16,7 +18,6 @@ dict5 = unpickle("./data/cifar-10-batches-py/data_batch_5")
 test = unpickle("./data/cifar-10-batches-py/test_batch")
 meta_data = unpickle("./data/cifar-10-batches-py/batches.meta")
 label_names = meta_data["label_names"]
-print(label_names)
 
 
 X_train = np.concatenate((dict1["data"],dict2["data"],dict3["data"],dict4["data"],dict5["data"]))
@@ -34,8 +35,9 @@ def plot_image(image, title=""):
     plt.show()
 
 # as a verification that everything is working correctly, plot an image
-image_nr = 10
+image_nr = 3141
 plot_image(X_train[image_nr,:],label_names[y_train[image_nr]])
+
 
 ## B - Fitting a logistic Regression model
 model = linear_model.LogisticRegression(penalty="l2",C=1e-9).fit(X_train, y_train)
@@ -48,11 +50,11 @@ print(label_names[0])
 
 ## C - Crossvalidation
 # note that in the choosing of folds, there is some randomness
-X_train = X_train[:100,:]
-y_train = y_train[:100]
+X_train_temp = X_train[:1000,:]
+y_train_temp = y_train[:1000]
 K = 4
-C_list = [1e-7,1e-8,1e-9,1e-10]
-model = linear_model.LogisticRegressionCV(cv = K, Cs = C_list).fit(X_train,y_train)
+C_list = [1e-6,1e-7,1e-8,1e-9,1e-10]
+model = linear_model.LogisticRegressionCV(cv = K, Cs = C_list).fit(X_train_temp,y_train_temp)
 fold_accuracies = model.scores_[0]
 mean_accuracies = np.mean(fold_accuracies, axis=0)
 print(fold_accuracies)
@@ -62,3 +64,28 @@ plt.figure()
 plt.loglog(C_list,mean_accuracies)
 plt.show()
 
+## D - Crossvalidation with logarithmic scoring rule
+K = 4
+C_list = [1e-6,1e-7,1e-8,1e-9,1e-10]
+acc_arr = np.array([])
+for C in C_list:
+    model = linear_model.LogisticRegression(C=C)
+    acc = model_selection.cross_val_score(model, X=X_train_temp, y=y_train_temp, cv=K, scoring="neg_log_loss")
+    mean_acc = np.mean(acc)
+    acc_arr = np.append(acc_arr, -mean_acc) # The sign is flipped in order to get the correct log loss
+
+print(C_list)
+print(acc_arr) #Smaller score is better
+plt.figure()
+plt.loglog(C_list,acc_arr)  
+plt.show()
+
+## E - Optimal value for C
+opt_C = 1e-7
+model = linear_model.LogisticRegression(C=opt_C).fit(X=X_train,y=y_train)
+print(model.score(X_train,y_train))
+print(model.score(X_test,y_test))
+pred_train = model.predict_proba(X_train)
+pred_test = model.predict_proba(X_test)
+print(metrics.log_loss(y_true=y_train, y_pred=pred_train))
+print(metrics.log_loss(y_true=y_test, y_pred=pred_test))
