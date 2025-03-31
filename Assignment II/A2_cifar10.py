56c0be207ccf196e4e11a5547c7fd3334ad12dc5
LarsdeHoop <134620883+LarsdeHoop@users.noreply.github.com>
2025-03-31 17:07:27 +0200

    Update A2_cifar10.py
    Trained on full data. Added warning suppression. Changed printing statements

--- Assignment II/A2_cifar10.py
+++ Assignment II/A2_cifar10.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Mar 26 14:11:04 2025
+Created on Wed Mar 26 12:21:05 2025
 
-@author: lucas
+@authors:    Yazan Mash'Al (5443768),
+            Lars de Hoop (5644690),
+            Lucas Verbeeke (5650534)
 """
 
 import numpy as np 
@@ -10,6 +11,10 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn import model_selection
 from sklearn import metrics
+
+# Disable warnings for failed convergence
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 def unpickle(file):
     import pickle
@@ -48,25 +53,22 @@ plot_image(X_train[image_nr,:],label_names[y_train[image_nr]])
 
 ## B - Fitting a logistic Regression model
 model = linear_model.LogisticRegression(penalty="l2",C=1e-8).fit(X_train, y_train)
-result = model.predict(X_test)
-print(sum(result == y_test))
-print(len(y_test))
-print(X_train.shape)
+print(f"Accuracy on X_train (C=1e-8): {model.score(X_train,y_train)*100}%")
 
-print(label_names[0])
 
 ## C - Crossvalidation
-# note that in the choosing of folds, there is some randomness
-X_train_temp = X_train[:1000,:]
-y_train_temp = y_train[:1000]
+
 K = 4
 C_list = [1e-6,1e-7,1e-8,1e-9,1e-10]
-model = linear_model.LogisticRegressionCV(cv = K, Cs = C_list).fit(X_train_temp,y_train_temp)
+print(f"\nTried values of C:\n{C_list}")
+model = linear_model.LogisticRegressionCV(cv = K, Cs = C_list).fit(X_train,y_train)
 fold_accuracies = model.scores_[0]
 mean_accuracies = np.mean(fold_accuracies, axis=0)
-print(fold_accuracies)
+# print(fold_accuracies)
+print("\nAverage accuracy per C:")
 print(mean_accuracies)
 
+# loglog plot of accuracy against C
 plt.figure()
 plt.loglog(C_list,mean_accuracies)
 plt.xlabel("C")
@@ -74,18 +76,22 @@ plt.ylabel("Accuracy")
 plt.title("Accuracy of the model for different values of C")
 plt.show()
 
+
 ## D - Crossvalidation with logarithmic scoring rule
 K = 4
 C_list = [1e-6,1e-7,1e-8,1e-9,1e-10]
 acc_arr = np.array([])
 for C in C_list:
     model = linear_model.LogisticRegression(C=C)
-    acc = model_selection.cross_val_score(model, X=X_train_temp, y=y_train_temp, cv=K, scoring="neg_log_loss")
+    acc = model_selection.cross_val_score(model, X=X_train, y=y_train, cv=K, scoring="neg_log_loss")
     mean_acc = np.mean(acc)
     acc_arr = np.append(acc_arr, -mean_acc) # The sign is flipped in order to get the correct log loss
 
-print(C_list)
-print(acc_arr) #Smaller score is better
+
+print("\nAverage log-loss per C:")
+print(acc_arr)
+
+#loglog plot of log loss against C:
 plt.figure()
 plt.loglog(C_list,acc_arr)
 plt.xlabel("C")
@@ -93,12 +99,14 @@ plt.ylabel("Log loss")
 plt.title("Log loss of the model for different values of C")
 plt.show()
 
-## E - Optimal value for C
+# ## E - Optimal value for C
 opt_C = 1e-7
 model = linear_model.LogisticRegression(C=opt_C).fit(X=X_train,y=y_train)
-print(model.score(X_train,y_train))
-print(model.score(X_test,y_test))
+
+print("\nFor C=1e-7:")
+print(f"Train accuracy: {model.score(X_train,y_train)}")
+print(f"Test accuracy: {model.score(X_test,y_test)}")
 pred_train = model.predict_proba(X_train)
 pred_test = model.predict_proba(X_test)
-print(metrics.log_loss(y_true=y_train, y_pred=pred_train))
-print(metrics.log_loss(y_true=y_test, y_pred=pred_test))
+print(f"Train log-loss: {metrics.log_loss(y_true=y_train, y_pred=pred_train)}")
+print(f"Test log-loss: {metrics.log_loss(y_true=y_test, y_pred=pred_test)}")
